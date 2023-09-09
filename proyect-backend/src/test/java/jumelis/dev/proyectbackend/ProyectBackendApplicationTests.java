8e9f879f273a8f1b5bbefe5daf4f7a27015fa8d8
Jumelis Rosales <jumelisrosales@gmail.com>
2023-09-09 18:43:14 +0200

    initial commit

---
+++ proyect-backend/src/test/java/jumelis/dev/proyectbackend/ProyectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package jumelis.dev.proyectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
