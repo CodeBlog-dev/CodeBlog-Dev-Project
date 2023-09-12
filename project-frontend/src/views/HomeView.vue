760a667c9d629987366a42fc3aea74750034329f
jumelis <jumelisrosales@gmail.com>
2023-09-12 21:17:09 +0200

    Refactoring of components and views, backend call logic

---
+++ project-frontend/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import ArticleCardComponent from '../components/home/ArticleCardComponent.vue';
+</script>
+
+<template>
+  <ArticleCardComponent />
+  <main>
+  </main>
+</template>
